14a57c8de7866ecbe8af7931e0f260f520f326d7
sinth <sinth@DESKTOP-T3MVRNP>
2016-07-22 12:52:24 +0200

    Base struktur angular js views

---
+++ SchoolProject/Scripts/School/PunctationQuestion.js
@@ -0,0 +1,8 @@
+﻿(function () {
+	var app = angular.module("app");
+
+	app.controller("PunctationQuestionController", function ($scope) {
+		$scope.test = "hello world";
+	});
+
+}());
---
+++ SchoolProject/Scripts/School/SentenceQuestion.js
@@ -0,0 +1,8 @@
+﻿(function () {
+	var app = angular.module("app");
+
+	app.controller("SentenceQuestionController", function ($scope) {
+		$scope.test = "hello world";
+	});
+
+}());
