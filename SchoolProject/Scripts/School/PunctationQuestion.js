efeff6942524bcc14187e81d77c1900574903f69
AnttiLatomaa <antti.j.latomaa@gmail.com>
2016-08-08 16:02:55 +0200

    PunctuationQuestion: mer pillande

--- SchoolProject/Scripts/School/PunctationQuestion.js
+++ SchoolProject/Scripts/School/PunctationQuestion.js
@@ -7,20 +7,22 @@
         //ex. "Hej, hur är läget? Roligt att höra!".replace(/[\,,\.,\?,\!]/g, "*"); -> "Hej* hur är läget* Roligt att höra*"
 
         // $scope.qg = questionGenerator;
-        var checkAnswer, question, answer, questionGenerator, isCorrectAnswer = false;
+        var question,
+            answer,
+            checkAnswer,
+            noOfPChars,
+            isCorrectAnswer = false;
 
+        var punctuationSentences = ["Hej!", "Hej! Hur är läget?", "Mors lilla Olle i skogen gick. Rosor på kinden, solsken i blick!"];
         var punctuationCharsRegExp = /[\,,\.,\?,\!]/g;   // Todo: Lägg till fler
         var replaceChar = "*";
-        var punctuationSentences = ["Hej!", "Hej! Hur är läget?", "Mors lilla Olle i skogen gick. Rosor på kinden, solsken i blick!"];
 
         $scope.answer = punctuationSentences[0];
         $scope.question = function () {
-            console.log("Inne i questionGenerator!");
             return $scope.answer.replace(punctuationCharsRegExp, replaceChar);
         }();
 
         $scope.checkAnswer = function () {
-            console.log("Inne i checkAnswer!");
             if ($scope.answer == $scope.userInput) {
                 $scope.isCorrectAnswer = true
             }
