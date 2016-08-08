0cfff887bf138a5bdf5c489b3fbbc6c6608d63d0
AnttiLatomaa <antti.j.latomaa@gmail.com>
2016-08-08 10:47:10 +0200

    PunctuationQuestion: Gjorde klart logiken

--- SchoolProject/Scripts/School/PunctationQuestion.js
+++ SchoolProject/Scripts/School/PunctationQuestion.js
@@ -7,26 +7,28 @@
         //ex. "Hej, hur är läget? Roligt att höra!".replace(/[\,,\.,\?,\!]/g, "*"); -> "Hej* hur är läget* Roligt att höra*"
 
         // $scope.qg = questionGenerator;
-        var question, answer, questionGenerator;
+        var checkAnswer, question, answer, questionGenerator, isCorrectAnswer = false;
 
         var punctuationCharsRegExp = /[\,,\.,\?,\!]/g;   // Todo: Lägg till fler
         var replaceChar = "*";
-        var punctuationSentences = ["Hej! Hur är läget?", "Mors lilla Olle i skogen gick. Rosor på kinden, solsken i blick!"];
+        var punctuationSentences = ["Hej!", "Hej! Hur är läget?", "Mors lilla Olle i skogen gick. Rosor på kinden, solsken i blick!"];
 
-        $scope.answer = punctuationSentences[1];
+        $scope.answer = punctuationSentences[0];
         $scope.question = function () {
             console.log("Inne i questionGenerator!");
             return $scope.answer.replace(punctuationCharsRegExp, replaceChar);
         }();
 
-        //app.filter("questionGenerator", function () {
-        //    return function (input) {
-        //        console.log("Inne i questionGenerator!");
-
-        //        output = $scope.answer.replace(punctuationCharsRegExp, replaceChar);
-        //        return output;
-        //    }
-        //});
+        $scope.checkAnswer = function () {
+            console.log("Inne i checkAnswer!");
+            if ($scope.answer == $scope.userInput) {
+                $scope.isCorrectAnswer = true
+            }
+            else {
+                $scope.isCorrectAnswer = false
+            }
+            return isCorrectAnswer;
+        };
 
     });
 
