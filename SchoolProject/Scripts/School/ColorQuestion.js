2c3999caa7aef72f7e416da2af5dfa367de53a9d
matseriksson <eriksson_e_mats@hotmail.com>
2016-08-08 15:25:17 +0200

    Namnbyte på variabel

--- SchoolProject/Scripts/School/ColorQuestion.js
+++ SchoolProject/Scripts/School/ColorQuestion.js
@@ -3,9 +3,9 @@
 
 	app.controller("ColorQuestionController", function ($scope) {
 		$scope.test = "Klicka på rutan med samma färg som den här texten!";
-		$scope.color = randomWebColor();                                    //Syntax ex: "#FF00FF";
+		$scope.color = randomColor();                                    //Syntax ex: "#FF00FF";
 
-		function randomWebColor(){
+		function randomColor(){
 		    var colors = new Array("00","FF");
 		    return '#'+colors[Math.floor(Math.random()*2)]+colors[Math.floor(Math.random()*2)]+colors[Math.floor(Math.random()*2)]
 		}
